87fd1bf8cff2bd6e534101af517f292ab7eef762
Joe S <honestcomrade@gmail.com>
2018-10-03 17:44:24 -0700

    refactor to insert the ascii char so we can actually print it

--- interview/01_06-stringCompression/main.go
+++ interview/01_06-stringCompression/main.go
@@ -14,12 +14,17 @@ import (
 // if the "compressed" string would be longer
 // than the input string, return the input string
 // assume the string has only lower case (a-z)
-func StringCompression(x string) []byte {
+func StringCompression(x string) string {
 	replaceCount := 0
-	chars := []byte(x) // format the input string into a slice of bytes
-	var retStr []byte  //  make an output slice of bytes to return (as a string later)
+	chars := []rune(x) // format the input string into a slice of bytes
+	var retStr []rune  //  make an output slice of bytes to return (as a string later)
 	// retStr = append(retStr, chars[0])
 	for i := 0; i < len(chars); i++ {
+		// early return for if we ever make a string
+		// that would be longer than the input
+		if len(retStr) > i {
+			return string(chars)
+		}
 		if i == 0 {
 			// chars[0] always goes in
 			retStr = append(retStr, chars[i])
@@ -30,25 +35,22 @@ func StringCompression(x string) []byte {
 		// that means we need to count the replacement ticker up
 		if chars[i-1] == chars[i] {
 			replaceCount++
-			// special case for last char, need to append replacecount right away
+			// special case for last char always need to insert replaceCount
 			if i == len(chars)-1 {
-				retStr = append(retStr, byte(replaceCount+1))
+				retStr = append(retStr, rune(replaceCount+1)+'0') // convert the int to it's ascii equiv
 			}
 		} else {
 			// otherwise since we are onto a new char we need to append
-			// the old char and the replacecount
-			retStr = append(retStr, byte(replaceCount+1))
+			// the old char and the replaceCount
+			retStr = append(retStr, rune(replaceCount+1)+'0')
 			retStr = append(retStr, chars[i])
 			replaceCount = 0
 		}
 	}
-	return retStr
+	return string(retStr)
 }
 
 func main() {
 	str := StringCompression("aaaabbaaa")
-	for _, char := range str {
-		fmt.Printf("%c", char)
-	}
-	// fmt.Println(str)
+	fmt.Println(str)
 }
