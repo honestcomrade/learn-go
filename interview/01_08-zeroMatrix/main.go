15e0dd3d0c5f858290dc31a34055e2a3a8934204
Joe S <honestcomrade@gmail.com>
2018-10-04 11:13:02 -0700

    implementation that works with more than one zero

--- interview/01_08-zeroMatrix/main.go
+++ interview/01_08-zeroMatrix/main.go
@@ -7,26 +7,46 @@ import "fmt"
 // its entire row and column are set to 0
 func ZeroMatrix(x [][]int) [][]int {
 	n := len(x)
-	zeroRow := 0
-	zeroCol := 0
+	// create a slice of boleans that each index will refer
+	// to a row or col that we found a zero at
+	zr := make([]bool, n)
+	zc := make([]bool, len(x[0]))
+
 	for i := 0; i < n; i++ {
-		for j := i; j < n; j++ {
+		for j := 0; j < len(x[0]); j++ {
 			if x[i][j] == 0 {
-				// save the location that we found the 0 at index
-				zeroRow = i
-				zeroCol = j
-				fmt.Print("Found a zero at:", zeroRow, ":", zeroCol, "\n")
+				// save the row that we found a 0 at in the bool array of rows
+				zr[i] = true
+				// save the col that we found a 0 at in the bool array of cols
+				zc[j] = true
 			}
 		}
 	}
-	// loop through again and zero out everything at that row or col
-	for i := range x {
-		for j := range x[i] {
-			x[zeroRow][j] = 0
-			x[i][zeroCol] = 0
+	// loop through again and zero out everything at that row
+	for r, v := range zr {
+		if v == true {
+			zeroRow(x, r)
+		}
+	}
+	// same as above but for cols
+	for c, v := range zc {
+		if v == true {
+			zeroCol(x, c)
 		}
 	}
 	return x
+}
+
+func zeroRow(x [][]int, r int) {
+	for c := range x[0] {
+		x[r][c] = 0
+	}
+}
+
+func zeroCol(x [][]int, c int) {
+	for r := range x {
+		x[r][c] = 0
+	}
 }
 
 func main() {
@@ -34,7 +54,13 @@ func main() {
 		{1, 1, 2, 2},
 		{1, 1, 0, 4},
 		{1, 1, 2, 9},
-		{1, 1, 2, 9},
+	}
+	for i := range matrix {
+		fmt.Print("\n")
+		for _, v2 := range matrix[i] {
+
+			fmt.Print(v2, " ")
+		}
 	}
 	zeroedMatrix := ZeroMatrix(matrix)
 	for i := range zeroedMatrix {
@@ -44,5 +70,4 @@ func main() {
 			fmt.Print(v2, " ")
 		}
 	}
-	fmt.Println(ZeroMatrix(matrix))
 }
